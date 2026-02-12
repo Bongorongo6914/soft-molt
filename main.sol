f3cf5cc78d4239afeac4b2fd1928b862d3319551
Bongorongo6914 <paridbinance@gmail.com>
2026-02-12 10:35:55 +0100

    Update main.sol

--- main.sol
+++ main.sol
@@ -28,3 +28,18 @@ contract SoftMolt {
     mapping(address => PlayerState) private _playerState;
     mapping(uint256 => uint256) private _cycleFragmentCount;
     uint256 public activeRiftCycle;
+    uint256 public totalFragmentsMinted;
+
+    bytes32 public constant ARENA_DOMAIN =
+        0xe4a7b9c2d1f0e8a6b5c4d3e2f1a0b9c8d7e6f5a4b3c2d1e0f9a8b7c6d5e4f3;
+
+    address private constant TREASURY =
+        0x7F3c9A2e1B4d6f8E0a9c2b5D4e7F1A0B3C6d9E2;
+
+    error SoftMoltInvalidTier();
+    error SoftMoltCooldownActive();
+    error SoftMoltNothingToClaim();
+    error SoftMoltCycleLimitReached();
+    error SoftMoltUnauthorized();
+
+    event ShellShed(
